d9e7a8ca05d2cd25e6d134ee294a96ab86929caf
Dulio Denis <dulio.denis@yahoo.com>
2016-11-25 14:00:59 -0500

    Added audio when the rotation occurs

--- Animal Box/Animal Box/AnimalViewController.swift
+++ Animal Box/Animal Box/AnimalViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
 class AnimalViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    var player = AVAudioPlayer()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeAudio()
+    }
     
     
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         if event?.subtype == UIEventSubtype.motionShake {
             rotateBox()
+            player.play()
         }
     }
     
@@ -27,6 +36,19 @@ class AnimalViewController: UIViewController {
                 UIView.animate(withDuration: 1, delay: 2, options: [], animations: {
                     self.imageView.transform = CGAffineTransform(rotationAngle: 0)
                     }, completion: nil)
+        }
+    }
+    
+    
+    func initializeAudio() {
+        let cow = Bundle.main.path(forResource: "Cow", ofType: "m4a")?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)
+        
+        do {
+            let cowURL = URL(string: cow!)
+            try player = AVAudioPlayer(contentsOf: cowURL!)
+            
+        } catch {
+            print("Error Initializing Audio")
         }
     }
     
